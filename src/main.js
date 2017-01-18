288d5433a93ef650f440c4774cd8ccdfae440269
Jason Blais <jason@spinpunch.com>
2017-01-18 09:26:56 -0500

    Update default window dimensions to 1000x700

--- src/main.js
+++ src/main.js
@@ -386,8 +386,8 @@ app.on('ready', function() {
     window_options = JSON.parse(fs.readFileSync(bounds_info_path, 'utf-8'));
   }
   catch (e) {
-    // follow Electron's defaults
-    window_options = {};
+    // Follow Electron's defaults, except for window dimensions which targets 1024x768 screen resolution.
+    window_options = {width: 1000, height: 700};
   }
   if (process.platform === 'linux') {
     window_options.icon = path.resolve(__dirname, 'resources/appicon.png');
